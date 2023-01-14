3341fa9c7ed970c1c442d0a32c2922e8dd091345
Victor-Amarante <vafstats@gmail.com>
2023-01-14 11:48:50 -0300

    init commit

---
+++ src/app.py
@@ -0,0 +1,7 @@
+# Load libraries
+import pandas as pd
+import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
